a2def3823ff4e60602d6818ce9e1096244b1e5a1
Chetan Gupta <chetangupta89@gmail.com>
2015-12-21 04:10:37 +0530

    moved user index to ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
+    respond_to :js
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @users }
+    # end
   end
 
   # GET /users/1
