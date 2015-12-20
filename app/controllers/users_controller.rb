fc6641eaa3a394bbff1b1d4a539413e784432e99
Chetan Gupta <chetangupta89@gmail.com>
2015-12-21 03:54:24 +0530

    Settin defaults if don't exit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -115,6 +115,8 @@ class UsersController < ApplicationController
 
   def casino_profit
     @casino_win_loss_hash = Game.where('state = ?', 'over').group(:is_won).sum(:bet_amount)
+    @casino_win_loss_hash[false] ||= 0
+    @casino_win_loss_hash[true] ||= 0
     @profit = @casino_win_loss_hash[false] - @casino_win_loss_hash[true] * 2
   end
 
